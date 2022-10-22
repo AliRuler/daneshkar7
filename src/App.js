b11d1d1726de45503b570a8f63234f6a1b220846
[Soheil1400] <[soheil.saedi@ymail.com]>
2022-10-22 20:23:53 +0330

    add redux-persist

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import {RouterProvider} from "react-router-dom";
 import router from "./router/router";
 import {Provider} from 'react-redux'
-import {store} from "./redux/store";
+import {persistor, store} from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
 
 function App() {
     return (
         //provider like sun on city, you can see sun everywhere
         <Provider store={store}>
-            <RouterProvider router={router}/>
+            <PersistGate loading={null} persistor={persistor}>
+                <RouterProvider router={router}/>
+            </PersistGate>
         </Provider>
     );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,22 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import ContactSlice from "./slice/contact.reducer";
+import {persistStore, persistReducer} from "redux-persist";
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const allReducer = combineReducers({
+    contacts: ContactSlice,
+})
+
+const allPersistReducer = persistReducer(persistConfig, allReducer)
 
 export const store = configureStore({
     //reducer are my products
-    reducer: {
-        contacts: ContactSlice,
-    }
+    reducer: allPersistReducer
 })
+
+export const persistor = persistStore(store)
